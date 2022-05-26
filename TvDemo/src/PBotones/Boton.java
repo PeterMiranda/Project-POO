46aac12c9ac5669cc15e0ad8d39eae8abf75a430
Luis Gerardo Garcia <104441673+JerryG96@users.noreply.github.com>
2022-05-26 15:30:12 -0500

    Update Boton.java
    Comentarios

--- TvDemo/src/PBotones/Boton.java
+++ TvDemo/src/PBotones/Boton.java
@@ -3,11 +3,16 @@ package PBotones;
 import PMain.Tv;
 
 /**
+ * Luis Gerardo Garcia Obispo (Documentador)
+ * Implementación de la clase Boton
+ * Se agregarón las variables de MIN_VOLUME y MAX_VOLUME para
+ * denotar cuando se sube y baja el volumen en tv, asignandoles un 
+ * volúmen definido. 
  * Class Boton
  */
-public abstract class Boton {
-    //@daniel suarez nava
-    //Manuel Melchor Gaspar
+//Daniel Suárez Nava (Coder)
+//Juan Manuel Melchor Gaspar (Coder)
+    public abstract class Boton {
     public int MIN_VOLUME = 0;
     public int MAX_VOLUME = 10;
     //public int volume = 6;
