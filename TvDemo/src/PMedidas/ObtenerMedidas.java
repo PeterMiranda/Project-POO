4ef258a91f144a72bfd29b8506741218ff4e767e
angelhz <atorresh879@alumno.uaemex.mx>
2022-05-30 23:10:43 -0500

    Update ObtenerMedidas.java
    Se realizo el constructor para inicalizar los compomentes de la clase ObtenerMedida

--- TvDemo/src/PMedidas/ObtenerMedidas.java
+++ TvDemo/src/PMedidas/ObtenerMedidas.java
@@ -1,4 +1,5 @@
 package PMedidas;
+import java.awt.*;
 import java.text.DecimalFormat;
 import javax.swing.*;
 
@@ -19,7 +20,43 @@ public class ObtenerMedidas extends JFrame {
     //Angel Torres
     //constructor
     public ObtenerMedidas(){
+        this.dim = new Dimension(100,50,25, new MedidaInternacional());
+        centPanel = new JPanel(new GridLayout(3,0));
+        infPanel = new JPanel(new FlowLayout());
 
+        btnCGS = new JButton("Medida CGS");
+        btnCGS.addActionListener(e->actualizaMedidaCGS());
+        btnInt = new JButton("Medida internacioanl");
+        btnInt.addActionListener(e->actualizaMedidaInt());
+
+        infPanel.add(btnInt);
+        infPanel.add(btnCGS);
+
+        labelDiagonal = new JLabel();
+        labelAncho = new JLabel();
+        labelLargo = new JLabel();
+
+        labelDiagonal.setFont(new Font(labelDiagonal.getName(),Font.PLAIN,50));
+        labelAncho.setFont(new Font(labelAncho.getName(),Font.PLAIN,50));
+        labelLargo.setFont(new Font(labelLargo.getName(),Font.PLAIN,50));
+        actualizaMedidas("CM");
+        labelDiagonal.setHorizontalAlignment(0);
+        labelAncho.setHorizontalAlignment(0);
+        labelLargo.setHorizontalAlignment(0);
+
+
+        centPanel.add(labelDiagonal);
+        centPanel.add(labelAncho);
+        centPanel.add(labelLargo);
+
+        this.getContentPane().setLayout(new BorderLayout());
+        this.getContentPane().add(centPanel, BorderLayout.CENTER);
+        this.getContentPane().add(infPanel, BorderLayout.SOUTH);
+        this.setSize(800,600);
+        this.setTitle("Obtener medidas");
+        this.setLocationRelativeTo(null);
+        //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 
     //metodos
