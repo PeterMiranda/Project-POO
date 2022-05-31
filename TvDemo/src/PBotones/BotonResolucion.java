9153c9442788fca4876ec96f98d521b0b1769d00
angelhz <atorresh879@alumno.uaemex.mx>
2022-05-30 21:46:33 -0500

    Se agregaron nuevas clases.
    Se agrego la clase BotonResolucion en el paquete PBotones para desplegar la informacion de las medidas de la TV desde un JFrame. Tambien se agrego la clase ObtnerMedidas en el paquete PMedidas la cual es un JFrame que muestra las medidas de la TV.

---
+++ TvDemo/src/PBotones/BotonResolucion.java
@@ -0,0 +1,20 @@
+package PBotones;
+
+import PMain.Tv;
+
+public class BotonResolucion extends Boton {
+
+    //constructor
+    //Implementar el constructor de esta clase
+    //David Abner
+    public BotonResolucion(Tv tv){
+
+    }
+
+    //Crear una instnacia de la clase ObtnerMedias
+    //David Abner
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ TvDemo/src/PMedidas/ObtenerMedidas.java
@@ -0,0 +1,49 @@
+package PMedidas;
+
+import javax.swing.*;
+
+public class ObtenerMedidas extends JFrame {
+
+    //atributos para crear la ventana que mostrara las medidas de la tv
+    private JPanel centPanel;
+    private JPanel infPanel;
+
+    private JLabel labelDiagonal;
+    private JLabel labelLargo;
+    private JLabel labelAncho;
+
+    private JButton btnInt;
+    private JButton btnCGS;
+    private Dimension dim;
+
+    //Angel Torres
+    //constructor
+    public ObtenerMedidas(){
+
+    }
+
+    //metodos
+
+    //Jaqueline Cruz
+    //implementar un metodo para actualizar los datos de las medidas a Internacional en pantalla
+    private void actualizaMedidaInt(){
+
+    }
+    //implementar un metodo para actualizar los datos de las medidas a CGS en pantalla
+    //Anotonio ibarra
+    private void actualizaMedidaCGS(){
+
+    }
+
+    //implementar un metodo para obtener los cambios de medida desde un objeto de la clase Dimension
+    //Angel Torres
+    private void actualizaMedidas(String unidad) {
+
+    }
+
+    //autodemo
+    public static void main(String[] args) {
+        new ObtenerMedidas();
+    }
+
+}
